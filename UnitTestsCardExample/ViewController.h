6427c6326ca0d66d386c05c8dc3ff004a0ee140f
zac1st1k <zacfirst@gmail.com>
2015-02-13 15:57:37 +1100

    Initial Commit

---
+++ UnitTestsCardExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitTestsCardExample
+//
+//  Created by Zac on 13/02/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UnitTestsCardExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnitTestsCardExample
+//
+//  Created by Zac on 13/02/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
